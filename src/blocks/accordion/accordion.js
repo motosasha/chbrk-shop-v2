af3038f8f43498c49c09c0c8d931c54768c24905
motosasha <moto.sasha@gmail.com>
2024-03-16 11:59:28 +0300

    feat: add validation

--- src/blocks/accordion/accordion.js
+++ src/blocks/accordion/accordion.js
@@ -6,19 +6,15 @@ ready(function () {
 
   if (accordionHost) {
     const accordionItems = accordionHost.querySelectorAll(".accordion__item");
-
     accordionItems.forEach((item) => {
       const accordionHead = item.querySelector(".accordion__head");
-
       accordionHead.addEventListener("click", (event) => {
         accordionItems.forEach((elem) => {
           elem.classList.remove("accordion__item--active");
         });
-
         const parentElem = event.target.parentElement;
-
         parentElem.classList.add("accordion__item--active");
-        parentElem.scrollIntoView();
+        parentElem.scrollIntoView({ behavior: "smooth", block: "start" });
       });
     });
   }
---
+++ src/js/common/dict.js
@@ -0,0 +1,50 @@
+export const dict = [
+  {
+    key: "Value is incorrect",
+    dict: {
+      ru: "Неверное значение",
+    },
+  },
+  {
+    key: "The field is required",
+    dict: {
+      ru: "Поле не может быть пустым",
+    },
+  },
+  {
+    key: "Name is required",
+    dict: {
+      ru: "Имя обязательно",
+    },
+  },
+  {
+    key: "Name is too short",
+    dict: {
+      ru: "Имя слишком короткое",
+    },
+  },
+  {
+    key: "Name is too long",
+    dict: {
+      ru: "Имя слишком длинное",
+    },
+  },
+  {
+    key: "Email is required",
+    dict: {
+      ru: "Почта обязательна",
+    },
+  },
+  {
+    key: "Email is invalid",
+    dict: {
+      ru: "Адрес почты неверен",
+    },
+  },
+  {
+    key: "Message should be more than 3 letters",
+    dict: {
+      ru: "Сообщение должно быть более 3 символов",
+    },
+  },
+];
